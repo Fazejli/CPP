ffd0a0e08bc157ffdfcc170b92badae3e0df21ba
Fazejli <125263457+Fazejli@users.noreply.github.com>
2026-01-09 16:33:56 +0000

    ok

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,13 +4,13 @@
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    void(av);
+    PhoneBook phonebook;
     std::string input;
     int         index;
 
     while (1)
     {
+        std::cout << "Enter a command: " << std::endl;
         std::cin.ignore(input);
         if (input == "ADD")
             addContact();
